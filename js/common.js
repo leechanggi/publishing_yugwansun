9ca8c38c271a38b1c38c27456c42767a33963978
leechanggi <lxyex1379@naver.com>
2020-11-23 13:55:09 +0900

    formal
    2020-11-23_13:55

--- js/common.js
+++ js/common.js
@@ -17,32 +17,48 @@ $(function () {
     //row3-calendar(datepicker)
     $(function calendarFnc() {
         $("#row3-calendar").datepicker({
-            dayNames : ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+            dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
             dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-            monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
-            monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+            monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+            monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
             currentText: 'Today',
             showOtherMonths: true
         });
     });
-    $(function subslideFnc(){
+    //row4-subslide(bxSlider)
+    $(function subslideFnc() {
         $('.row4-subslide').bxSlider({
-        slideWidth: 320,
-        moveSlides: 1,
-        minSlides: 1,
-        maxSlides: 3,
-        auto: true,
-        useCSS: true,
-        shrinkItems: true,
-        slideMargin: 30,
-        pager: false,
-        controls : true
+            slideWidth: 320,
+            moveSlides: 1,
+            minSlides: 1,
+            maxSlides: 3,
+            auto: true,
+            useCSS: true,
+            shrinkItems: true,
+            slideMargin: 30,
+            pager: false,
+            controls: true
         });
     });
-    // 호출
+    //all-menu-btn
+    $(function openmenu() {
+        $('#btnAllmenu').click(function () {
+            var menu = document.getElementById("allmenuInfo");
+            menu.style.top = "0";
+        })
+    })
+    $(function closemenu() {
+        $('#btnAllmenuClose').click(function () {
+            var menu = document.getElementById("allmenuInfo");
+            menu.style.top = "-100vh";
+        })
+    })
+    // 호출문
     $(function () {
         gnbFnc();
         calendarFnc();
         subslideFnc();
+        openmenu();
+        closemenu();
     })
 });
