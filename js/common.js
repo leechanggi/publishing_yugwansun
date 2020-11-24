14009ebad371c4785650c3a2a2e955f76e9125d9
leechanggi <lxyex1379@naver.com>
2020-11-25 00:08:01 +0900

    formal
    2020-11-25_00:07

--- js/common.js
+++ js/common.js
@@ -14,42 +14,6 @@ function gnbFnc() {
         })
 }
 
-//row3-calendar(datepicker)
-var calendar;
-
-function calendarFnc() {
-    calendar = $("#row3-calendar").datepicker({
-        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
-        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-        monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-        showOtherMonths: true,
-        dateFormat: "yy" + "년 " + "mm" + "월 " + "dd" + "일",
-        onSelect: function (seletday, inst) {
-            $('.row3-today').html(seletday);
-        },
-    });
-    var Day = (calendar.datepicker("getDate"));
-    $('.row3-today').html(Day.getFullYear() + '년 ' + (Day.getMonth() + 1) + '월 ' + Day.getDate() + '일');
-    Day.getDate();
-}
-
-//row4-subslide(bxSlider)
-function subslideFnc() {
-    $('.row4-subslide').bxSlider({
-        slideWidth: 320,
-        moveSlides: 1,
-        minSlides: 1,
-        maxSlides: 3,
-        auto: true,
-        useCSS: true,
-        shrinkItems: true,
-        slideMargin: 30,
-        pager: false,
-        controls: true
-    });
-}
-
 //all-menu-btn
 function openmenu() {
     $('#btnAllmenu').click(function () {
@@ -89,8 +53,6 @@ function btnLang() {
 //호출문
 $(function () {
     gnbFnc();
-    calendarFnc();
-    subslideFnc();
     openmenu();
     closemenu();
     btnSns();
