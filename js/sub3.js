c88e5275f56c963d9dbedaf6345ac898862cb167
leechanggi <lxyex1379@naver.com>
2020-12-02 22:50:29 +0900

    formal
    2020-12-2_22:50

--- js/sub3.js
+++ js/sub3.js
@@ -4,7 +4,7 @@ $(function () {
         dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
         monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
         monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-        numberOfMonths: 2,
+        numberOfMonths: 1,
         showButtonPanel: true
     });
     $("#edate").datepicker({
@@ -12,7 +12,7 @@ $(function () {
         dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
         monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
         monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-        numberOfMonths: 2,
+        numberOfMonths: 1,
         showButtonPanel: true
     });
 });
