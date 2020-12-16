05ebeaa3fd0e3fbc55a540f470147671caa4389a
leechanggi <lxyex1379@naver.com>
2020-12-16 12:12:06 +0900

    formal
    2020/12/16_12:12

--- js/sub5.js
+++ js/sub5.js
@@ -7,9 +7,9 @@ function agreeMainFnc() {
     $('.agreeMain').click(() => {
             if ($('.agree')[0].hasAttribute('checked') == false) {
                 alert('회원가입약관 및 개인정보처리방침안내 약관에 동의하셔야 회원가입 하실 수 있습니다.');
-            }else{
-                window.location.replace('../sub6.html')
-            }
+            }//else{
+                //window.location.replace('../sub6.html')
+            //}
         })
 }
 $(function () {
